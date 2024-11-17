f2f670d68cabe47f2446e22f7b420b8dd1a578fe
Staicey Brooks <staicey@brooks.bz>
2024-11-17 22:22:58 +0000

    Updates dependencies

--- nvim/_install.sh
+++ nvim/_install.sh
@@ -6,7 +6,7 @@ installPackage gcc make golang python3
 case "$(uname -a)" in
     *Darwin*) installPackage neovim virtualenv ;;
     *Debian*)
-        installPackage libfuse2 python3-venv
+        installPackage fuse3 python3-venv
 
         curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
         chmod u+x nvim.appimage
